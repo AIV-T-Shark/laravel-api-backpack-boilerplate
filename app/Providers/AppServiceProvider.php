d3db78b815a8c1d26a8d2fdc3a0d453cbbb72156
Nguyen Gia Hao <giahao9899@gmail.com>
2021-08-26 00:29:58 +0700

    add backpack

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        $request_uri = \Request::getRequestUri();
+        
+        //admin
+        $admin_prefix = '/'.config('backpack.base.route_prefix', 'admin');
+        if (mb_strpos($request_uri, $admin_prefix) === 0 || $this->app->runningInConsole()) {
+            $this->app->register(\Backpack\CRUD\BackpackServiceProvider::class);
+            $this->app->register(\Backpack\LogManager\LogManagerServiceProvider::class);
+            $this->app->register(\Backpack\PermissionManager\PermissionManagerServiceProvider::class);
+    
         }
         
+        //api
         
     }
 
