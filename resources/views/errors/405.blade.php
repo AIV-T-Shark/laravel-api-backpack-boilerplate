e1b5aa006176b3b9d3af22abbbb34d01d7012581
Nguyen Gia Hao <giahao9899@gmail.com>
2021-09-03 15:09:48 +0700

    change error pages

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -1,16 +1,5 @@
-@extends('errors.layout')
+@extends('errors::minimal')
 
-@php
-  $error_number = 405;
-@endphp
-
-@section('title')
-  Method not allowed.
-@endsection
-
-@section('description')
-  @php
-    $default_error_message = "Please <a href='javascript:history.back()''>go back</a> or return to <a href='".url('')."'>our homepage</a>.";
-  @endphp
-  {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
-@endsection
+@section('title', __('Method not allowed'))
+@section('code', '401')
+@section('message', __('Method not allowed'))
